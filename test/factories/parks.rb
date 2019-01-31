c64853d36695a9fc32a1eec7801bcbbad7f170f3
Richard Roepke <RAERoepke@gmail.com>
2019-01-31 14:43:58 -0500

    Add Park Model

---
+++ test/factories/parks.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :park do
+    username "MyString"
+    report false
+    name "MyString"
+    address "MyString"
+    city "MyString"
+    zipcode "MyString"
+    state_id 1
+    phone "MyString"
+    email "MyString"
+  end
+end
