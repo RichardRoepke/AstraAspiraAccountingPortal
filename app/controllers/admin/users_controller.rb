8622e5963fe93d0febb0914e25737d7e9a675137
Richard Roepke <RAERoepke@gmail.com>
2019-01-31 16:09:46 -0500

    Improving User Display

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,30 +1,13 @@
 class Admin::UsersController < AdminController
   def index
-    @content_header = 'All Users'
     @sidebar = 'users:index'
+    @content_header = 'All Users'
     @user_list = User.all
   end
 
-  def edit
-    @sidebar = 'users:edit'
-    @user = User.find(params[:id])
-    @content_header = 'Edit ' + @user.name
-  end
-
   def show
-    @user = User.find(params[:id])
-    @content_header = @user.username  + ' Information'
     @sidebar = 'users:info'
-    @payment = @user.payments.last
-  end
-
-  def update
     @user = User.find(params[:id])
-    if @user.update(params.require(:user).permit(:name, :address, :phone, :email, :active, :logo, :sync))
-      flash[:success] = "User updated."
-      redirect_to :back
-    else
-      redirect_to edit_admin_user_path(@park)
-    end
+    @content_header = @user.email  + ' Information'
   end
 end
