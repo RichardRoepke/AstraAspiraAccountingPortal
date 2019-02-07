d4eb177b73d9f7d3503bc67932402e9920674926
Richard Roepke <RAERoepke@gmail.com>
2019-02-07 16:34:22 -0500

    Add Search to Reports Index

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,17 @@
 class ReportsController < ApplicationController
   def index
     @sidebar = 'reports:index'
-    @content_header = 'All Reports'
-    @report_list = Report.reporting_parks_only
+    @start_date = params[:start_date] || (Date.today - 3.week)
+    @end_date = params[:end_date] || Date.today
+
+    if @end_date <= @start_date
+      @start_date = (Date.today - 3.week)
+      @end_date = Date.today
+      flash[:alert] = 'Start date was after or before end date. Search has been reset.'
+    end
+
+    @content_header = 'All Reports from ' + @start_date.to_s + " to " + @end_date.to_s
+    @report_list = Report.reporting_parks_only.where("start_date >= ?", @start_date).where("end_date <= ?", @end_date)
   end
 
   def show
